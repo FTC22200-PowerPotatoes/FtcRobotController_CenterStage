b374a427e759d200b10cea258475048c91bc8a29
ZCodingKid <ZCodingKid@gmail.com>
2023-10-01 10:48:33 -0400

    Made the slow mode actully work. Changed velocity of normal driving.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMode.java
@@ -16,6 +16,9 @@ public class DriveMode extends LinearOpMode {
         DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRight = hardwareMap.dcMotor.get("backRight");
 
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+
         waitForStart();
         if (isStopRequested()) return;
 
@@ -24,17 +27,13 @@ public class DriveMode extends LinearOpMode {
             double x = gamepad1.left_stick_x * 1.1; // The 1.1 multiplier is to counteract imperfect strafing
             double rx = gamepad1.right_stick_x; // Turning left/right
 
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1); /* makes sure motor values don't go outside of [-1,1] */
-            double fL_Motor = (y+x+rx)/denominator; // fL = FrontLeft
-            double bL_Motor = (y-x+rx)/denominator; // bL = BackLeft
-            double fR_Motor = (y-x-rx)/denominator; // fR = FrontRight
-            double bR_Motor = (y+x-rx)/denominator; // bR = backRight
 
-            frontLeft.setPower(fL_Motor);
-            backLeft.setPower(bL_Motor);
-            frontRight.setPower(fR_Motor);
-            backRight.setPower(bR_Motor);
 
+
+            double fL_Motor = Range.clip((y+x+rx), -0.8, 0.8); // fL = FrontLeft
+            double bL_Motor = Range.clip((y-x+rx), -0.8, 0.8); // bL = BackLeft
+            double fR_Motor = Range.clip((y-x-rx), -0.8, 0.8); // fR = FrontRight
+            double bR_Motor = Range.clip((y+x-rx), -0.8, 0.8); // bR = backRight
 
             /*
              - While holding the right bumper on gamepad 1, the robot goes in a 'slow mode'
@@ -42,17 +41,24 @@ public class DriveMode extends LinearOpMode {
              - Values can be adjusted based on feedback from drivers
             */
 
-            while (gamepad1.right_bumper) {
-                fL_Motor = Range.clip((y+x+rx), -0.5, 0.5);// Using the clip feature in order to activate 'slow mode'
-                bL_Motor = Range.clip((y-x+rx), -0.5, 0.5);// Using the clip feature in order to activate 'slow mode'
-                fR_Motor = Range.clip((y-x-rx), -0.5,0.5); // Using the clip feature in order to activate 'slow mode'
-                bR_Motor = Range.clip((y+x-rx), -0.5,0.5); // Using the clip feature in order to activate 'slow mode'
+            if (gamepad1.right_bumper) {
+                fL_Motor = Range.clip((y+x+rx), -0.2, 0.2);// Using the clip feature in order to activate 'slow mode'
+                bL_Motor = Range.clip((y-x+rx), -0.2, 0.2);// Using the clip feature in order to activate 'slow mode'
+                fR_Motor = Range.clip((y-x-rx), -0.2,0.2); // Using the clip feature in order to activate 'slow mode'
+                bR_Motor = Range.clip((y+x-rx), -0.2,0.2); // Using the clip feature in order to activate 'slow mode'
 
-                frontLeft.setPower(fL_Motor);
-                backLeft.setPower(bL_Motor);
-                frontRight.setPower(fR_Motor);
-                backRight.setPower(bR_Motor);
             }
+
+
+            frontLeft.setPower(fL_Motor);
+            backLeft.setPower(bL_Motor);
+            frontRight.setPower(fR_Motor);
+            backRight.setPower(bR_Motor);
+
+
+
+
+
 
         }
     }
